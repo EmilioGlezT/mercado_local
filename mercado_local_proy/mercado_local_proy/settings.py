10370052ac7eb59aea7aebcc3a13b747be22a2e7
Emilio Gonzalez <guimelemilio@gmail.com>
2023-11-16 20:04:10 -0600

    Agregando app productos en settings

--- mercado_local_proy/mercado_local_proy/settings.py
+++ mercado_local_proy/mercado_local_proy/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mercado_local_app',
+    'productos_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
