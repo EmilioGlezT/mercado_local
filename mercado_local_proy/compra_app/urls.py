39efddcda3ec83ad84c14bcdd0a2c3f56c624679
José Luis Reyes Chávez <joseluis.reyeschavez@gmail.com>
2023-11-29 16:09:31 -0600

    Carrito funcionando

--- mercado_local_proy/compra_app/urls.py
+++ mercado_local_proy/compra_app/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
-from compra_app.views import Carrito_ListView
+from compra_app.views import ver_carrito, agregar_al_carrito
 
 app_name = "compra_app"
 urlpatterns = [
-    path('carrito/', Carrito_ListView.as_view(), name='carrito'),
+     path('agregar_al_carrito/<int:producto_id>/', agregar_al_carrito, name='agregar_al_carrito'),
+     path('carrito/', ver_carrito, name='ver_carrito'),
+    ##path('carrito/', Carrito_ListView.as_view(), name='carrito'),
 ]
--- mercado_local_proy/compra_app/views.py
+++ mercado_local_proy/compra_app/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from productos_app.models import Producto
 from compra_app.models import Orden 
 
 # Create your views here.
-class Carrito_ListView(ListView):
-    template_name = 'compra_app/carrito.html'
-    context_object_name = 'carrito'
+##
+##class Carrito_ListView(ListView):
+  ##  template_name = 'compra_app/carrito.html'
+    ##context_object_name = 'carrito'
 
-    def get_queryset(self):
-        return Orden.objects.all()
+    ##def get_queryset(self):
+      ##  return Orden.objects.all()
+
+def ver_carrito(request):
+    # Obtener todas las órdenes en el carrito
+    carrito = Orden.objects.filter(venta__isnull=True)
+    
+    context = {'carrito': carrito}
+    return render(request, 'compra_app/carrito.html', context)
+
+def agregar_al_carrito(request, producto_id):
+    # Obtener el producto a agregar al carrito
+    producto = Producto.objects.get(id=producto_id)
+    
+    # Obtener o crear la orden en el carrito para ese producto
+    orden, creado = Orden.objects.get_or_create(
+        producto=producto,
+        en_carrito=True,
+        defaults={'cantidadProducto': 1, 'costoOrden': producto.precio}
+    )
+
+    if not creado:
+        # Si la orden ya existe en el carrito, aumentar la cantidad y actualizar el costo
+        orden.cantidadProducto += 1
+        orden.costoOrden += producto.precio
+        orden.save()
+
+    # Redirigir a la página de detalle del producto o a donde desees
+    return redirect('compra_app:ver_carrito')
